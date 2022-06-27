4d425b7ef6fbf03bd0a7de395f0377a19387be42
nanarama <annasampers@gmail.com>
2022-06-27 13:01:52 -0500

    Add '+' to current_guess to keep length the same. Gamelogic works now. Need to fix incorrect letters not being uniq

--- main.rb
+++ main.rb
@@ -14,33 +14,39 @@ module GameLogic
   def update_word_progress(secret_word, current_guess, word_progress)
     secret_word.each_with_index do |ltr, i|
       if current_guess[i] == secret_word[i]
-        word_progress[i] = "#{ltr}"
-        current_guess.delete(ltr)
+        word_progress[i] = "#{ltr} "
+        current_guess[i] = "+"
       end
     end 
-    puts word_progress.join
-    current_guess   
+    puts "Game progress: #{word_progress.join}"
+    return current_guess   
   end
 
   def update_ltrs_guessed(secret_word, current_guess, ltrs_guessed)
     current_guess.each_with_index.map do |ltr, i|
-      if answer.include?(current_guess[i])
-        current_guess.delete(ltr) 
+      if secret_word.include?(current_guess[i])
+        current_guess[i] = "+"
       end
-    end  
-    current_guess
-    ltrs_guessed << (current_guess - ltrs_guessed)
-    puts ltrs_guessed.join
+    end 
+    current_guess.delete("+") 
+    current_guess.uniq!
+    ltrs_to_add = current_guess - ltrs_guessed
+    ltrs_guessed << ltrs_to_add
+    ltrs_guessed
+    puts "Incorrect letters: #{ltrs_guessed.join.to_s.upcase}"
   end
 
-  def won?(secret_word, current_guess, name)
-    if secret_word.join == current_guess.join
-      "Congratulations, #{name}! You guessed correctly!"
+  def won?(secret_word, word_progress, player, turn)
+    if secret_word.join == word_progress.join.delete(" ")
+      puts "Secret word: #{secret_word.join}"
+      puts "Congratulations, #{player}! You guessed correctly!"
+      turn -= 6
+      return turn
     end 
   end  
 
-  def lost(secret_word, name)
-    "Sorry, #{name}. You ran out of guesses. The word was '#{secret_word}'."   
+  def lost(secret_word, player)
+    puts "Sorry, #{player}. You ran out of guesses. The word was '#{secret_word.join}'."   
   end
 end
 
@@ -52,29 +58,33 @@ class Game
 
   def initialize
     @ltrs_guessed = []
-    @secret_word = populate_dictionary.split("")
-    puts "The Secret word is #{@secret_word.join}"
-    @word_progress = Array.new(@secret_word.length - 1, " __ ")
-    puts "The word progress is #{@word_progress.join}"
     @player = HumanPlayer.new
     puts "The player is #{@player}"
+    @secret_word = populate_dictionary.split("")
+    puts "The Secret word is #{@secret_word}"
+    @word_progress = Array.new(@secret_word.length, " __ ")
+    puts "The secret word is #{@secret_word.length} letters long."
+    puts "#{@word_progress.join}\n"
   end
 
   def play
     turn = 6
     while turn > 0
-      puts "#{turn} turns left"
-      @current_guess = @player.make_guess
-      puts "The current guess is #{@current_guess}"
-      update_word_progress(@secret_word, @current_guess, @word_progress)
-      update_ltrs_guessed(@secret_word, @current_guess, @ltrs_guessed)
-      if won?(@secret_word, @current_guess, @player)
-        return
+      puts "----#{turn} turns left----"
+      current_guess = HumanPlayer.make_guess
+      update_word_progress(@secret_word, current_guess, @word_progress)
+      if won?(@secret_word, @word_progress, @player, turn)
+        then break
       end  
+      update_ltrs_guessed(@secret_word, current_guess, @ltrs_guessed)
       turn -= 1
+      if turn == 0
+        lost(@secret_word, @player)
+      end  
     end 
-    lost(@secret_word, @player)
+    
   end
+
 end  
 
 class HumanPlayer
@@ -89,18 +99,17 @@ class HumanPlayer
     name
   end
 
-  def make_guess
+  def self.make_guess
     loop do
       puts "Please guess a 5-12 letter word."
       current_guess = gets.chomp.downcase
         if current_guess.length > 4 && current_guess.length < 13
-          break
+          return current_guess = Array(current_guess.split('') )     
         else
-          "That's not the right amount of letters."
-        end
-      return current_guess = Array(current_guess.split('') )  
+          puts "That's not the right amount of letters."
+        end 
     end
-           
+       
   end
 
   def to_s
