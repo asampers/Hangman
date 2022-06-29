383b7e1c9bdd5713ada88f96f7b7844b46534fa4
nanarama <annasampers@gmail.com>
2022-06-29 12:26:43 -0500

    load_game works, can play thru but gives 1 extra turn

--- main.rb
+++ main.rb
@@ -51,7 +51,7 @@ module SavedGames
   def save_game(game)
     puts "...saving game..."
     Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
-    filename = "saved_games/game#{Dir.glob('../saved_games/*').length+1}.yml"
+    filename = "saved_games/game#{Dir.glob('./saved_games/*').length+1}.yml"
 
     File.open(filename, 'w') {|file| file.puts YAML.dump(game)}
     puts "Game saved. Thanks for playing"
@@ -74,6 +74,9 @@ module SavedGames
     @player = f.player
     @word_progress = f.word_progress
     @turn = f.turn
+    puts "#{@word_progress.join}"
+    puts "Incorrect letters: #{@ltrs_guessed.join}"
+    File.delete("#{filename}")
   end
 
 end
@@ -91,8 +94,7 @@ class Game
       answer = gets.chomp.upcase
       if answer == "Y"
         choose_saved_game()
-        play()
-        return
+        return play()
       end
     end 
     @ltrs_guessed = []
@@ -109,14 +111,14 @@ class Game
   def play
     while @turn != 0
       puts "----#{@turn} turns left---- If you'd like to save this game enter 'Save'."
-      current_guess = HumanPlayer.make_guess(@ltrs_guessed, @word_progress)
+      current_guess = HumanPlayer.make_guess(@ltrs_guessed)
       if current_guess == 'save'
         save_game(self)
         break
       end  
       update_word_progress(@secret_word, current_guess, @word_progress)
       if won?(@secret_word, @word_progress, @player, @turn)
-        then break
+        break
       end  
       if update_ltrs_guessed(@secret_word, current_guess, @ltrs_guessed) == true
         @turn -=1
@@ -142,19 +144,19 @@ class HumanPlayer
     name
   end
 
-  def self.make_guess(ltrs_guessed, word_progress)
+  def self.make_guess(ltrs_guessed)
     loop do
       puts "Please guess a letter."
       current_guess = gets.chomp.downcase
         if current_guess == 'save'
           return current_guess 
-        elsif ltrs_guessed.include?(current_guess) || word_progress.include?(current_guess)  
+        elsif ltrs_guessed.include?(current_guess)
           puts "You've already guessed that letter."
         elsif current_guess.length > 1 || current_guess.count('a-z').zero?
           puts "Incorrect input. Please try again."
         else
           return current_guess  
-        end 
+        end   
     end
        
   end
